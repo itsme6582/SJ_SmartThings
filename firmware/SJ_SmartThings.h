d5c42fc260ac96b78d7694878c6e2c9b614f2053
itsme6582 <jesse.weiland@gmail.com>
2016-02-17 21:23:22 -0500

    Update SJ_SmartThings.h

--- firmware/SJ_SmartThings.h
+++ firmware/SJ_SmartThings.h
@@ -1,1 +1,20 @@
+#include "application.h"
 
+#ifndef SJ_SmartThings_h
+#define SJ_SmartThings_h
+
+class ST_Switch {
+    public:
+        ST_Switch();
+        void begin(String id, byte pin);
+        void capability(String command);
+        void capability(String command, String id);
+        
+    private:
+        void _on();
+        void _off();
+        byte _pin;
+        String _id;
+};
+
+#endif
